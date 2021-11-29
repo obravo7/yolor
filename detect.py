907961baa4a95e6a8d94b05c7375408b39924701
eric <narciso.eric@plus-zero.co.jp>
2021-11-29 17:32:01 -0500

    bug fix: allow cpu inference

--- detect.py
+++ detect.py
@@ -21,11 +21,13 @@ from models.models import *
 from utils.datasets import *
 from utils.general import *
 
+
 def load_classes(path):
     # Loads *.names file at 'path'
     with open(path, 'r') as f:
         names = f.read().split('\n')
     return list(filter(None, names))  # filter removes empty strings (such as last line)
+
 
 def detect(save_img=False):
     out, source, weights, view_img, save_txt, imgsz, cfg, names = \
@@ -40,7 +42,9 @@ def detect(save_img=False):
     half = device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
-    model = Darknet(cfg, imgsz).cuda()
+    # model = Darknet(cfg, imgsz).cuda()
+    # allow model init with CPU
+    model = Darknet(cfg, imgsz) if device.type == 'cpu' else Darknet(cfg, imgsz).cuda()
     model.load_state_dict(torch.load(weights[0], map_location=device)['model'])
     #model = attempt_load(weights, map_location=device)  # load FP32 model
     #imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
